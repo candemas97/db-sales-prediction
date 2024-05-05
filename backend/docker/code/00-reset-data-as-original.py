1fd20226b36ce4bb6dd39267003fdab735866cc8
cargar_github <candemas97@hotmail.com>
2024-05-04 23:14:44 -0500

    Backend - Redis and MongoDB

---
+++ backend/docker/code/00-reset-data-as-original.py
@@ -0,0 +1,157 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pymongo
+import pandas as pd
+
+import os
+import json
+import dotenv
+
+
+def cargar_valores_env() -> str:
+    """Leer todas las variables secretas
+
+    Returns:
+        str: Retorna las variables secretas
+    """
+
+    dotenv.load_dotenv()
+    USERNAME = os.getenv("MONGODB_USER")
+    PASSWORD = os.getenv("MONGODB_PASSWORD")
+    SERVER = os.getenv("MONGODB_SERVER")
+    return USERNAME, PASSWORD, SERVER
+
+
+def reset_opportunity_data(**context) -> None:
+    """
+    Borrar variables de MongoDB
+    """
+
+    USERNAME, PASSWORD, SERVER = context["task_instance"].xcom_pull(
+        task_ids="cargar_valores_env"
+    )
+
+    # Conectar a la base de datos en MongoDB
+    uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@{SERVER}.czf3nfw.mongodb.net/?retryWrites=true&w=majority&appName={SERVER}"
+
+    # Crear cliente y conectar al servidor
+    client = MongoClient(uri, server_api=ServerApi("1"))
+
+    # Enviar PING para conocer si hay conexión  exitosa
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    # Conectarse a la base de datos
+    db = client["final_project"]
+
+    # Conectarse a la colección
+    collection_oppty = db["opportunities"]
+    # Borrar lo que se encuentra en la colección
+    collection_oppty.delete_many({})
+
+    # Se lee la data
+    df_oportunidad = pd.read_csv(
+        f"{os.getcwd()}/data/oportunidades_negocio_projecto_final_bases_de_datos.csv"
+    )
+
+    # Insertar datos del DataFrame a MongoDB
+    ## Se pasa el DataFrame a diccionario dirigido por records
+    records = df_oportunidad.to_dict("records")
+    ## Se manda a MongoDB
+    collection_oppty.insert_many(records)
+
+
+def reset_user_data(**context) -> None:
+    """
+    Cargar valores originales
+    """
+
+    USERNAME, PASSWORD, SERVER = context["task_instance"].xcom_pull(
+        task_ids="cargar_valores_env"
+    )
+
+    # Conectar a la base de datos en MongoDB
+    uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@{SERVER}.czf3nfw.mongodb.net/?retryWrites=true&w=majority&appName={SERVER}"
+
+    # Crear cliente y conectar al servidor
+    client = MongoClient(uri, server_api=ServerApi("1"))
+
+    # Enviar PING para conocer si hay conexión  exitosa
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    # Conectarse a la base de datos
+    db = client["final_project"]
+    # Conectarse a la colección
+    collection_users = db["users"]
+
+    # Borrar lo que se encuentra en la colección
+    collection_users.delete_many({})
+
+    # Lectura de datos originales
+    df_vendedores = pd.read_csv(
+        f"{os.getcwd()}/data/vendedores_projecto_final_bases_de_datos.csv"
+    )
+
+    # Insertar datos del DataFrame a MongoDB}
+
+    ## Se pasa el DataFrame a diccionario dirigido por records
+    records = df_vendedores.to_dict("records")
+    ## Se manda a MongoDB
+    collection_users.insert_many(records)
+
+
+# DAG creation and execution
+
+"""
+Crear dag e intervalo de ejecución
+"""
+dag = DAG(
+    "00-reset-data-as-original",
+    description="DAG que reinicia los datos como originalmente estaban",
+    start_date=datetime(2024, 5, 3, 0, 0, 00000),
+    schedule_interval="@once",
+    catchup=False,
+)
+
+"""
+Task 1: Leer valores .env
+"""
+t1 = PythonOperator(
+    task_id="cargar_valores_env",
+    provide_context=True,
+    python_callable=cargar_valores_env,
+    dag=dag,
+)
+
+"""
+Task 2: Reset datos de oportunidades
+"""
+t2 = PythonOperator(
+    task_id="reset_opportunity_data",
+    provide_context=True,
+    python_callable=reset_opportunity_data,
+    dag=dag,
+)
+
+"""
+Task 3: Reset datos de usuario
+"""
+t3 = PythonOperator(
+    task_id="reset_user_data",
+    provide_context=True,
+    python_callable=reset_user_data,
+    dag=dag,
+)
+
+t1 >> [t2, t3]
---
+++ backend/docker/code/01-train-model.py
@@ -0,0 +1,182 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pymongo
+import pandas as pd
+
+import os
+import dotenv
+import json
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer  # for dummies
+from sklearn.pipeline import Pipeline  # creating a pipeline
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+import joblib
+
+dotenv.load_dotenv()
+
+
+def read_data(USERNAME: str, PASSWORD: str, SERVER: str) -> json:
+    """Función que lee los datos
+
+    Args:
+        USERNAME (str): Usuario de la base de datos
+        PASSWORD (str): Contraseña de la base de datos
+        SERVER (str): Servidor de la base de datos
+
+    Returns:
+        json: Datos finales
+    """
+
+    uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@{SERVER}.czf3nfw.mongodb.net/?retryWrites=true&w=majority&appName={SERVER}"
+
+    # Crear cliente y conectar al servidor
+    client = MongoClient(uri, server_api=ServerApi("1"))
+
+    # Enviar PING para conocer si hay conexión  exitosa
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    # Conectarse a la base de datos
+    db = client["final_project"]
+
+    # Conectarse a la colección
+    collection_oppty = db["opportunities"]
+
+    # Leer datos excluyendo el campo '_id'
+    documents = collection_oppty.find(
+        {},
+        {
+            "_id": 0,
+            "unidad_negocio": 1,
+            "etapas": 1,
+            "tipos": 1,
+            "acv": 1,
+            "tcv": 1,
+            "margen_ganancia": 1,
+            "regiones": 1,
+            "territorios": 1,
+            "industrias": 1,
+            "edad_oferta": 1,
+            "lead_source": 1,
+        },
+    )
+
+    # Convertir a DataFrame
+    df = pd.DataFrame(list(documents))
+
+    return df.to_json(orient="records")
+
+
+def train_model(**context) -> None:
+    """
+    Entrena el modelo
+    """
+    # Tomar la data del paso anterior
+    data = context["task_instance"].xcom_pull(task_ids="read_data")
+    df = pd.read_json(data, orient="records")
+
+    # Solo tomar los que ya acabaron
+    df = df[df["etapas"].isin(["Lost", "Won"])]
+
+    # Division X y
+    y = df["etapas"]
+    X = df.drop(columns="etapas")
+
+    # (80% train, 20% test)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.20, random_state=42
+    )
+
+    # Dummificar
+
+    categorical_columns = X_train.select_dtypes(exclude=[int, float]).columns
+    column_trans = make_column_transformer(
+        (OneHotEncoder(handle_unknown="ignore"), categorical_columns),
+        remainder="passthrough",
+    )
+
+    # Escalar
+    pipe = Pipeline(
+        steps=[
+            ("column_trans", column_trans),
+            ("scaler", StandardScaler(with_mean=False)),
+            ("RandomForestClassifier", RandomForestClassifier()),
+        ]
+    )
+
+    # Búsqueda hiperparámetros
+    param_grid = dict()
+    param_grid["RandomForestClassifier__max_depth"] = [1, 2, 3, 10]
+    param_grid["RandomForestClassifier__n_estimators"] = [10, 11]
+
+    search = GridSearchCV(pipe, param_grid, cv=10, n_jobs=2)
+
+    # Entrenamiento
+    search.fit(X_train, y_train)
+
+    # Tomar el mejor modelo y grabar
+    best_estimator = search.best_estimator_
+    best_params = search.best_params_
+    print(f"\n\n\nLos mejores hiperparámetros fueron: {best_params}\n\n\n")
+    joblib.dump(best_estimator, f"{os.getcwd()}/model/ml_model.joblib")
+
+    # Verificar calidad de la solución
+    y_pred = best_estimator.predict(X_test)
+    print(
+        f"\n\n\nModel has an accuracy of {accuracy_score(y_test,y_pred)} in test\n\n\n"
+    )
+
+
+# DAG creation and execution
+
+"""
+Create dag and set the schedule interval
+"""
+dag = DAG(
+    "01-Train-Model",
+    description="DAG que entrena el modelo",
+    start_date=datetime(2024, 5, 3, 0, 0, 00000),
+    schedule_interval="@once",
+    catchup=False,
+)
+
+"""
+Task 1: Leer data guardada
+"""
+t1 = PythonOperator(
+    task_id="read_data",
+    provide_context=True,
+    python_callable=read_data,
+    op_kwargs={
+        "USERNAME": os.getenv("MONGODB_USER"),
+        "PASSWORD": os.getenv("MONGODB_PASSWORD"),
+        "SERVER": os.getenv("MONGODB_SERVER"),
+    },
+    dag=dag,
+)
+
+"""
+Task 2: Entrenar modelo
+"""
+t2 = PythonOperator(
+    task_id="train_model",
+    provide_context=True,
+    python_callable=train_model,
+    dag=dag,
+)
+
+t1 >> t2
